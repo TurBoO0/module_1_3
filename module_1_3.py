18c8e4bd36e5eb105debe3000540c33ede0c043b
TurBoO0 <127961357+TurBoO0@users.noreply.github.com>
2024-12-15 12:15:35 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Daniyar"
+print(f'Name: {name}')
+age = 27
+print (f'Age: {age}')
+age = age + 243
+print(f'Age after 243 years: {age}')
+is_student = True
+print(f'Is Student: {is_student}')
